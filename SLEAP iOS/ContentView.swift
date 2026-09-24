ea3df19d6228c52514bef56bda246262d8efd20c
surajladdagiri <152471106+surajladdagiri@users.noreply.github.com>
2026-09-24 13:05:12 -0700

    created simple displaying UI

--- SLEAP iOS/ContentView.swift
+++ SLEAP iOS/ContentView.swift
@@ -6,14 +6,43 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    private static let originalImage = UIImage(named: "reference.png")
+    private static let originalRenderedImage = UIImage(named: "reference_overlay.png")
+    @State private var displayedImage: UIImage? = ContentView.originalImage
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            
+            if let displayedImage{
+                Image(uiImage: displayedImage)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                                .strokeBorder(Color.green, lineWidth: 4)
+                    )
+            }else {
+                Text("Image could not be loaded")
+            }
+
+            HStack(spacing: 16) {
+                Button("Draw Points") {
+                        
+                }
+                Button("Reference"){
+                    displayedImage = Self.originalRenderedImage
+                }
+
+                Button("Reset") {
+                    displayedImage = Self.originalImage
+                }
+            }
+            .buttonStyle(.bordered)
         }
         .padding()
     }
---
+++ SLEAP iOS/PoseRenderer.swift
@@ -0,0 +1,23 @@
+//
+//  PoseRenderer.swift
+//  SLEAP iOS
+//
+//  Created by Suraj Laddagiri  on 9/24/26.
+//
+
+import SwiftUI
+import UIKit
+
+
+/*
+struct PoseRenderer{
+    
+    //isolate UI updates
+    @MainActor
+    func render(image: UIImage) -> UIImage{
+        let content = Image(Image: image)
+        return image
+    }
+}
+
+*/
