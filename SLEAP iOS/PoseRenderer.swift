e7d658dcfdafb546cb2194801739797ed0de75e5
surajladdagiri <152471106+surajladdagiri@users.noreply.github.com>
2026-09-24 14:31:24 -0700

    Finished image rendering pipeline

--- SLEAP iOS/PoseRenderer.swift
+++ SLEAP iOS/PoseRenderer.swift
@@ -29,16 +29,67 @@ struct PoseRenderer{
                     let colors: [Color] = [.cyan, .orange]
                     let radius: CGFloat = 3
                     
-                   
                     
                     
-                    
-                    
-                    //Add in the points
                     for (index, instance) in prediction.instances.enumerated() {
                         let color = colors[index % colors.count]
                         
-                        //check if point is valid
+                        
+                        
+                        //Add in edges
+                        for edge in prediction.edges {
+                            //ensure valid edges
+                            guard edge.count == 2 else{
+                                continue
+                            }
+                            
+                            
+                            let sourceIndex = edge[0]
+                            let destinationIndex = edge[1]
+                            
+                            //Get locations of source and destination points and ensure validity
+                            guard instance.points.indices.contains(sourceIndex),
+                                instance.points.indices.contains(destinationIndex),
+                                  let source = instance.points[sourceIndex],
+                                  let destination = instance.points[destinationIndex],
+                                  source.x.isFinite,
+                                  source.y.isFinite,
+                                  destination.x.isFinite,
+                                  destination.y.isFinite else {
+                                continue
+                            }
+                            
+                            
+                            //Map points back to image
+                            let start = CGPoint(
+                                x: CGFloat(source.x) * scaleX,
+                                y: CGFloat(source.y) * scaleY
+                            )
+                            let end = CGPoint(
+                                x: CGFloat(destination.x) * scaleX,
+                                y: CGFloat(destination.y) * scaleY
+                            )
+                            
+                            var line = Path()
+                            line.move(to: start)
+                            line.addLine(to: end)
+                            
+                            context.stroke(
+                                line,
+                                with: .color(color),
+                                style: StrokeStyle(
+                                    lineWidth: 2,
+                                    lineCap: .round
+                                )
+                            )
+                        }
+
+                        
+                        
+                        
+                        
+                        
+                        //Add in points
                         for optionalPoint in instance.points {
                             guard let point = optionalPoint,
                                   point.x.isFinite,
